a56405360c8a9603746eb6efa4d0a100185629ab
Johannes Heimansberg <git@jhe.dedyn.io>
2012-06-17 17:24:43 +0200

    Update dingux.mk to build the Dingux Gmu with the new OpenDingux toolchain

--- dingux.mk
+++ dingux.mk
@@ -18,11 +18,11 @@
 DECODERS_TO_BUILD=decoders/vorbis.so decoders/flac.so decoders/wavpack.so decoders/mpg123.so decoders/mikmod.so decoders/musepack.so
 FRONTENDS_TO_BUILD=frontends/sdl.so frontends/log.so
 SDL_LIB=-lSDL -lpthread -lpng -ljpeg
-SDL_CFLAGS=-I/usr/include/SDL -I/opt/mipsel-linux-uclibc/usr/include/ -D_GNU_SOURCE=1 -D_REENTRANT
-CXX=/opt/mipsel-linux-uclibc/usr/bin/mipsel-linux-g++
-CC=/opt/mipsel-linux-uclibc/usr/bin/mipsel-linux-gcc
-STRIP=/opt/mipsel-linux-uclibc/usr/bin/mipsel-linux-strip
+SDL_CFLAGS=-I/usr/include/SDL -I/opt/opendingux-toolchain/usr/include/ -D_GNU_SOURCE=1 -D_REENTRANT
+CXX=mipsel-linux-g++
+CC=mipsel-linux-gcc
+STRIP=mipsel-linux-strip
 COPTS?=-O3 -ffast-math
-CFLAGS=-fomit-frame-pointer $(SDL_CFLAGS) -I/opt/mipsel-linux-uclibc/usr/include/ -DFILE_HW_H="\"hw_$(TARGET).h\""
-LFLAGS=-s -L/opt/mipsel-linux-uclibc/usr/lib/ -lpthread -lm -ldl -lz -lgcc -Wl,-export-dynamic
+CFLAGS=-fomit-frame-pointer $(SDL_CFLAGS) -I/opt/opendingux-toolchain/usr/include/ -DFILE_HW_H="\"hw_$(TARGET).h\""
+LFLAGS=-s -L/opt/opendingux-toolchain/usr/lib/ -lpthread -lm -ldl -lz -lgcc -Wl,-export-dynamic
 DISTFILES=$(BINARY) frontends decoders themes gmu.png README.txt libs.dingoo gmu.dge COPYING gmuinput.dingux.conf gmu.bmp
