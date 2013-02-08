a832f529d528f9da44f29eb0983d61584674aff8
Johannes Heimansberg <git@jhe.dedyn.io>
2013-02-08 14:24:38 +0100

    Update pandora.mk to use Ivanovic's Pandora toolchain by default

--- pandora.mk
+++ pandora.mk
@@ -1,7 +1,7 @@
 # 
 # Gmu Music Player
 #
-# Copyright (c) 2006-2011 Johannes Heimansberg (wejp.k.vu)
+# Copyright (c) 2006-2013 Johannes Heimansberg (wejp.k.vu)
 #
 # File: pandora.mk  Created: 100601
 #
@@ -16,13 +16,13 @@
 
 #DECODERS_TO_BUILD=decoders/vorbis.so decoders/musepack.so decoders/flac.so decoders/wavpack.so decoders/mpg123.so decoders/mikmod.so
 DECODERS_TO_BUILD=decoders/wavpack.so decoders/mpg123.so decoders/vorbis.so
-FRONTENDS_TO_BUILD=frontends/sdl.so frontends/log.so
-SDL_LIB=-L/usr/local/pandora/arm-2009q3/lib -lSDL -lpthread
-SDL_CFLAGS=-I/usr/local/pandora/arm-2009q3/include/SDL -I/usr/local/pandora/arm-2009q3/include -D_GNU_SOURCE=1 -D_REENTRANT
-CXX=/usr/local/pandora/arm-2009q3/bin/arm-none-linux-gnueabi-g++
-CC=/usr/local/pandora/arm-2009q3/bin/arm-none-linux-gnueabi-gcc
-STRIP=/usr/local/pandora/arm-2009q3/bin/arm-none-linux-gnueabi-strip
+FRONTENDS_TO_BUILD=frontends/sdl.so frontends/log.so frontends/gmuhttp.so
+SDL_LIB=-L$(PNDSDK)/usr/lib -lSDL -lpthread
+SDL_CFLAGS=-I$(PNDSDK)/usr/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT
+CXX=$(PNDSDK)/bin/arm-none-linux-gnueabi-g++
+CC=$(PNDSDK)/bin/arm-none-linux-gnueabi-gcc
+STRIP=$(PNDSDK)/bin/arm-none-linux-gnueabi-strip
 COPTS?=-O3 -mtune=cortex-a8 -ffast-math -march=armv7-a -mfpu=neon -mfloat-abi=softfp
-CFLAGS=-fno-strict-aliasing -fomit-frame-pointer $(SDL_CFLAGS) -I/usr/local/pandora/arm-2009q3/include
-LFLAGS=-L/usr/local/pandora/arm-2009q3/lib -lts -lpng -ljpeg -lpthread -lm -ldl -lz -lgcc -Wl,-export-dynamic
+CFLAGS=-fno-strict-aliasing -fomit-frame-pointer $(SDL_CFLAGS) -I$(PNDSDK)/usr/include
+LFLAGS=-L$(PNDSDK)/usr/lib -lts -lpng -ljpeg -lpthread -lm -ldl -lz -lgcc -Wl,-export-dynamic
 DISTFILES=$(COMMON_DISTBIN_FILES) gmu.pandora.conf gmu-pandora.sh gmuinput.pandora.conf
