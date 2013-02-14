19f32f41650d0e98e2ee812722005583cf6ca253
Johannes Heimansberg <git@jhe.dedyn.io>
2013-02-14 21:03:10 +0100

    Update pandora.mk (PND build target)

--- pandora.mk
+++ pandora.mk
@@ -25,4 +25,18 @@ STRIP=$(PNDSDK)/bin/arm-none-linux-gnueabi-strip
 COPTS?=-O3 -mtune=cortex-a8 -ffast-math -march=armv7-a -mfpu=neon -mfloat-abi=softfp
 CFLAGS=-fno-strict-aliasing -fomit-frame-pointer $(SDL_CFLAGS) -I$(PNDSDK)/usr/include/ncursesw -I$(PNDSDK)/usr/include
 LFLAGS=-s -L$(PNDSDK)/usr/lib -lts -lpng -ljpeg -lpthread -lm -ldl -lz -lgcc -Wl,-export-dynamic
-DISTFILES=$(COMMON_DISTBIN_FILES) gmu.pandora.conf gmu-pandora.sh gmuinput.pandora.conf
+DISTFILES=$(COMMON_DISTBIN_FILES) gmuc gmu.pandora.conf gmuc.pandora.conf gmu-pandora.sh gmuc-pandora.sh gmuc-terminal-pandora.sh gmuinput.pandora.conf
+
+DESTDIR=tmp/
+PREFIX=pndgmu
+
+DISBIN_DEPS=pnd
+
+pnd: all distbin
+	mkdir -p tmp
+	-rm -rf tmp/$(projname)-$(TARGET)
+	-rm -rf tmp/gmu
+	unzip $(projname)-$(TARGET).zip -d tmp
+	mv tmp/$(projname)-$(TARGET) tmp/gmu
+	cp PXML.xml tmp/gmu
+	$(PNDSDK)/../sdk_utils/pnd_make.sh -p $(projname).pnd -i gmu.png -d `pwd`/tmp/gmu
