c1e07c9ee1905860c04d93e187779033aeacf8a0
Johannes Heimansberg <git@jhe.dedyn.io>
2013-02-17 21:17:21 +0100

    Update zipit-z2.mk to work with Zipit Z2 OpenWRT toolchain

--- zipit-z2.mk
+++ zipit-z2.mk
@@ -1,7 +1,7 @@
 # 
 # Gmu Music Player
 #
-# Copyright (c) 2006-2010 Johannes Heimansberg (wejp.k.vu)
+# Copyright (c) 2006-2013 Johannes Heimansberg (wejp.k.vu)
 #
 # File: zipit-z2.mk  Created: 100829
 #
@@ -15,14 +15,14 @@
 #
 
 #DECODERS_TO_BUILD=decoders/vorbis.so decoders/musepack.so decoders/flac.so decoders/wavpack.so decoders/mpg123.so decoders/mikmod.so
-DECODERS_TO_BUILD=decoders/wavpack.so decoders/vorbis.so decoders/mpg123.so decoders/flac.so decoders/speex.so
-FRONTENDS_TO_BUILD=frontends/sdl.so frontends/log.so
-SDL_LIB=-L/opt/crossdev/z2/lib -lSDL -lpthread
-SDL_CFLAGS=-I/home/wejp/crossdev/z2/buildroot/output/staging/usr/include/SDL -I/home/wejp/crossdev/z2/buildroot/output/staging/usr/include -D_GNU_SOURCE=1 -D_REENTRANT
-CXX=/home/wejp/crossdev/z2/buildroot/output/staging/usr/bin/arm-unknown-linux-uclibcgnueabi-g++
-CC=/home/wejp/crossdev/z2/buildroot/output/staging/usr/bin/arm-unknown-linux-uclibcgnueabi-gcc
-STRIP=/home/wejp/crossdev/z2/buildroot/output/staging/usr/bin/arm-unknown-linux-uclibcgnueabi-strip
+DECODERS_TO_BUILD=decoders/wavpack.so decoders/mpg123.so
+FRONTENDS_TO_BUILD=frontends/sdl.so frontends/gmuhttp.so
+SDL_LIB=-L$(ZIPIT_OPENWRT)/target-arm_v5te_uClibc-0.9.33.2_eabi/usr/lib -lSDL -lpthread
+SDL_CFLAGS=-I$(ZIPIT_OPENWRT)/target-arm_v5te_uClibc-0.9.33.2_eabi/usr/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT
+CXX=arm-openwrt-linux-g++
+CC=arm-openwrt-linux-gcc
+STRIP=arm-openwrt-linux-strip
 COPTS?=-O3 -mcpu=xscale -ffast-math
-CFLAGS=-fno-strict-aliasing -fomit-frame-pointer $(SDL_CFLAGS) -I/usr/local/pandora/arm-2009q3/include -DFILE_HW_H="\"hw_$(TARGET).h\""
-LFLAGS=-L/home/wejp/crossdev/z2/buildroot/output/staging/usr/lib -lpthread -lm -ldl -lz -lgcc -Wl,-export-dynamic
+CFLAGS=-fno-strict-aliasing -fomit-frame-pointer $(SDL_CFLAGS) -I$(ZIPIT_OPENWRT)/target-arm_v5te_uClibc-0.9.33.2_eabi/usr/include -DFILE_HW_H="\"hw_$(TARGET).h\""
+LFLAGS=-L$(ZIPIT_OPENWRT)/target-arm_v5te_uClibc-0.9.33.2_eabi/usr/lib -L$(ZIPIT_OPENWRT)/toolchain-arm_v5te_gcc-4.6-linaro_uClibc-0.9.33.2_eabi/lib -lpthread -lm -ldl -lz -lgcc -Wl,-export-dynamic
 DISTFILES=$(COMMON_DISTBIN_FILES) gmu-z2.sh gmuinput.zipit-z2.conf
