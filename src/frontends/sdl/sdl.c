614cbbead5d59324608b7e893d1a853ea4a4bf7c
Johannes Heimansberg <git@jhe.dedyn.io>
2013-05-30 17:01:27 +0200

    Easter egg update

--- src/frontends/sdl/sdl.c
+++ src/frontends/sdl/sdl.c
@@ -547,7 +547,7 @@ static void m_draw(M *m, SDL_Surface *t)
 		}
 		if (y < s) dy = s;
 	} else {
-		if (m->so > m->st) snprintf(tmp, 63, "%c %c %c", 53, 61, 66);
+		if (m->so > m->st) snprintf(tmp, 63, "%c %c %c", 54, 61, 89);
 		else snprintf(tmp, 63, "%c%cM%c%c%cV%cR", 71, 65, 69, 32, 48, 69);
 		textrenderer_draw_string_with_highlight(&skin.font1, &skin.font2, tmp, 0, t,
 		                                        (gmu_widget_get_width((GmuWidget *)&skin.lv, 1) - 
