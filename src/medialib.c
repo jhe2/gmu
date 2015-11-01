97839f7ab19d95f152235303760a391c54109b22
Johannes Heimansberg <git@jhe.dedyn.io>
2015-11-01 16:31:39 +0100

    medialib: Simplify medialib_path_add() function

--- src/medialib.c
+++ src/medialib.c
@@ -217,18 +217,15 @@ void medialib_refresh(GmuMedialib *gm)
 void medialib_path_add(GmuMedialib *gm, const char *path)
 {
 	sqlite3_stmt *pp_stmt = NULL;
-	int sqres;
-	/* Check if path has already been added ... */
-	char *q = "SELECT id FROM path WHERE path = ?1 LIMIT 1";
+	int           sqres;
+
+	const char *q =
+		"INSERT INTO path (path) " \
+		"SELECT ?1 " \
+		"WHERE NOT EXISTS (SELECT 1 FROM path WHERE path = ?1)";
 	sqres = sqlite3_prepare_v2(gm->db, q, -1, &pp_stmt, NULL);
 	if (sqres == SQLITE_OK) sqres = sqlite3_bind_text(pp_stmt, 1, path, -1, SQLITE_TRANSIENT);
-	if (sqres == SQLITE_OK && sqlite3_step(pp_stmt) != SQLITE_ROW) {
-		sqlite3_finalize(pp_stmt);
-		q = "INSERT INTO path (path) VALUE (?1)";
-		sqres = sqlite3_prepare_v2(gm->db, q, -1, &pp_stmt, NULL);
-		if (sqres == SQLITE_OK) sqres = sqlite3_bind_text(pp_stmt, 1, path, -1, SQLITE_TRANSIENT);
-		sqlite3_step(pp_stmt);
-	}
+	sqlite3_step(pp_stmt);
 	sqlite3_finalize(pp_stmt);
 }
 
