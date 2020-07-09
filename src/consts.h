581bb5a0c9de0bb5b2287331835b40237a9be286
Johannes Heimansberg <git@jhe.dedyn.io>
2020-07-09 19:40:34 +0200

    Add consts.h file

---
+++ src/consts.h
@@ -0,0 +1,10 @@
+#ifndef WEJ_CONSTS_H
+#define WEJ_CONSTS_H
+
+/* Total maximum length for a file path */
+#define PATH_LEN_MAX 512
+/* Maximum legth of the filename part of a file path */
+#define PATH_LEN_FILENAME_MAX 255
+/* Maximum length of the directory part of a file path */
+#define PATH_LEN_DIR_MAX 255
+#endif
