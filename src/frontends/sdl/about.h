cf21c3f5ae44c570846ce69e1a595ab74b745e1f
Johannes Heimansberg <git@jhe.dedyn.io>
2023-04-04 17:42:32 +0200

    sdl_frontend/core: Add more information to 'About Gmu' screen

--- src/frontends/sdl/about.h
+++ src/frontends/sdl/about.h
@@ -1,7 +1,7 @@
 /* 
  * Gmu Music Player
  *
- * Copyright (c) 2006-2010 Johannes Heimansberg (wejp.k.vu)
+ * Copyright (c) 2006-2023 Johannes Heimansberg (wejp.k.vu)
  *
  * File: about.h  Created: 061223
  *
@@ -19,5 +19,5 @@
 #ifndef _ABOUT_H
 #define _ABOUT_H
 int  about_process_action(TextBrowser *tb_about, View *view, View old_view, int user_key_action);
-void about_init(TextBrowser *tb_about, Skin *skin, char *decoders);
+void about_init(TextBrowser *tb_about, Skin *skin, const char *decoders);
 #endif
