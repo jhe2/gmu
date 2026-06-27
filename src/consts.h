50c355fd192475d1be21ba86da39ee130076dfdf
deeice <dheyse@hotmail.com>
2026-06-27 14:06:36 -0400

    Add support for https via tiny-curl lib

--- src/consts.h
+++ src/consts.h
@@ -7,4 +7,10 @@
 #define PATH_LEN_FILENAME_MAX 255
 /* Maximum length of the directory part of a file path */
 #define PATH_LEN_DIR_MAX 255
+
+#ifdef URL_WITH_CURL
+#define IS_URL(path) (strncasecmp((path), "http://", 7) == 0 || strncasecmp((path), "https://", 8) == 0)
+#else
+#define IS_URL(path) (strncasecmp((path), "http://", 7) == 0)
+#endif
 #endif
